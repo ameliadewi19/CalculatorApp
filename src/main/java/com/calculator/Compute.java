270a7f917cd96817a642787b327731bf66012632
ameliadewi19 <ameliadewi1929@gmail.com>
2024-04-07 17:15:13 +0700

    update compute

--- src/main/java/com/calculator/Compute.java
+++ src/main/java/com/calculator/Compute.java
@@ -5,22 +5,22 @@ public class Compute {
     public static void processOperation(int a, int b, String operator) {
         if (!InputValidator.isValidDivision(b, operator)) {
             System.out.println("Division by zero is not allowed.");
-            return;
-        }
-
-        switch (operator) {
-            case "+":
-                System.out.println("Result: " + Calculator.add(a, b));
-                break;
-            case "-":
-                System.out.println("Result: " + Calculator.subtract(a, b));
-                break;
-            case "*":
-                System.out.println("Result: " + Calculator.multiply(a, b));
-                break;
-            case "/":
-                System.out.println("Result: " + Calculator.divide(a, b));
-                break;
+        } else {
+            switch (operator) {
+                case "+":
+                    System.out.println("Result: " + Calculator.add(a, b));
+                    break;
+                case "-":
+                    System.out.println("Result: " + Calculator.subtract(a, b));
+                    break;
+                case "*":
+                    System.out.println("Result: " + Calculator.multiply(a, b));
+                    break;
+                case "/":
+                    System.out.println("Result: " + Calculator.divide(a, b));
+                    break;
+            }
         }
     }
 }
+
