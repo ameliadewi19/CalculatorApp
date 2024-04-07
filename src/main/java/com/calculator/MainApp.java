92b33761869b4c3902b4db19623183f1bf37d957
salsabilamp3 <salsabilamp3@gmail.com>
2024-04-07 20:00:04 +0700

    Fix main, remove loop input

--- src/main/java/com/calculator/MainApp.java
+++ src/main/java/com/calculator/MainApp.java
@@ -8,36 +8,26 @@ public class MainApp {
         int a = 0, b = 0;
         boolean validInput = false;
 
-        // Loop until valid numbers are input
-        while (!validInput) {
-            System.out.println("Enter two numbers (ex/ 2 8):");
-            String[] inputs = scanner.nextLine().split(" ");
+        System.out.println("Enter two numbers (ex/ 2 8):");
+        String[] inputs = scanner.nextLine().split(" ");
 
-            if (inputs.length != 2 || !InputValidator.isInteger(inputs[0]) || !InputValidator.isInteger(inputs[1])) {
-                System.out.println("Invalid input. Please enter valid numbers.");
+        if (inputs.length != 2 || !InputValidator.isInteger(inputs[0]) || !InputValidator.isInteger(inputs[1])) {
+            System.out.println("Invalid input. Please enter valid numbers.");
+        } else {
+            a = Integer.parseInt(inputs[0]);
+            b = Integer.parseInt(inputs[1]);
+            if (!InputValidator.isInRange(a) || !InputValidator.isInRange(b)) {
+                System.out.println("Numbers out of range. Please enter numbers in the range of -32,768 to 32,767.");
             } else {
-                a = Integer.parseInt(inputs[0]);
-                b = Integer.parseInt(inputs[1]);
-                if (!InputValidator.isInRange(a) || !InputValidator.isInRange(b)) {
-                    System.out.println("Numbers out of range. Please enter numbers in the range of -32,768 to 32,767.");
+                String operator = "";
+                System.out.println("Enter the operator (+, -, *, /):");
+                operator = scanner.next();
+                if (!InputValidator.isValidOperator(operator)) {
+                    System.out.println("Invalid operator. Only +, -, *, and / are allowed.");
                 } else {
-                    validInput = true; // Proceed only if both numbers are valid
+                    Compute.processOperation(a, b, operator);
                 }
             }
         }
-
-        String operator = "";
-        // Loop until a valid operator is input
-        while (true) {
-            System.out.println("Enter the operator (+, -, *, /):");
-            operator = scanner.next();
-            if (!InputValidator.isValidOperator(operator)) {
-                System.out.println("Invalid operator. Only +, -, *, and / are allowed.");
-            } else {
-                break; // Exit the loop if the operator is valid
-            }
-        }
-
-        Compute.processOperation(a, b, operator);
     }
 }
